266829e2f40d769f2385e73ee25b8cd672fb0ec5
marco bubola <marcobubola@hotmail.com>
2025-06-19 16:50:10 -0300

    Update ProdutoComponente.php

--- app/Models/ProdutoComponente.php
+++ app/Models/ProdutoComponente.php
@@ -5,6 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Model para componentes de kits de produtos.
+ * kit_produto_id: id do produto do tipo 'kit'
+ * componente_produto_id: id do produto componente
+ * quantidade: quantidade do componente no kit
+ */
 class ProdutoComponente extends Model
 {
     use HasFactory;
