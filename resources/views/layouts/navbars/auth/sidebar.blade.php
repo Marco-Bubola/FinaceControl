b50274d2ed05dabb68a39931df23e0feb34712aa
marco bubola <marcobubola@hotmail.com>
2025-06-20 18:46:27 -0300

    Update sidebar.blade.php

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -124,6 +124,14 @@
                     </a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link {{ (Request::is('cofrinho') ? 'active' : '') }}" href="{{ url('cofrinho') }}">
+                        <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-piggy-bank text-warning" style="font-size:1.5rem;"></i>
+                        </div>
+                        <span class="nav-link-text ms-1 sidebar-text">Cofrinhos</span>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link {{ (Request::is('products') ? 'active' : '') }}" href="{{ url('products') }}">
                         <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box2" viewBox="0 0 16 16">
